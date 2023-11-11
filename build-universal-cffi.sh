6582a81d0b114d950c0e6ac6092469e08f0e5529
John <thorrak@users.noreply.github.com>
2023-11-11 08:38:15 -0500

    Add CFFI build script Now required for ongoing universal2 binary support

---
+++ build-universal-cffi.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+source ./venv/bin/activate
+
+git clone https://github.com/python-cffi/cffi.git
+cd cffi
+git checkout v1.16.0
+
+python3 -m pip install build pytest
+
+# The proper SDKROOT can be found by running `xcrun --sdk macosx --show-sdk-path` -- right now, it's 14.0
+MACOSX_DEPLOYMENT_TARGET='11.0' SDKROOT=macosx14.0 python3 -m build -w .
+python3 -m pip install dist/cffi-*universal2.whl --force-reinstall
+
+# python3 -m pytest .
